02b366665d7b95ca9ce05de865c023f49bd90a33
Šimon Pekár <akantogaming@gmail.com>
2018-04-14 16:11:14 +0200

    Add second file, function double

---
+++ sec_test.cpp
@@ -0,0 +1,5 @@
+#include "sec_test.h"
+int double(int i)
+{
+	return 2*i;
+}
--- test.cpp
+++ test.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
-
+#include "sec_test.h"
 int main()
 {
 	using namespace std;
 	cout << "Test" << endl;
+	cout << double(5) << endl;
 	return 0;
 }
